3906b4da3ca7320c3ee3eea31052977604eed329
Tapas Mondal <mondaltapas02@gmail.com>
2023-11-07 21:01:46 +0530

    build remove

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>CI/CD TEST</p>
+        <p>CI/CD TEST update build in gitignore</p>
       </header>
     </div>
   );
