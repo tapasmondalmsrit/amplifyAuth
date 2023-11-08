c95644aead309a002e60110900e244656ec2aa27
Tapas Mondal <mondaltapas02@gmail.com>
2023-11-08 21:55:11 +0530

    authentication added

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "myapp29d2db630": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import { Amplify } from "aws-amplify";
 import logo from "./logo.svg";
 import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+
+import awsconfig from "./aws-exports";
+// import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { Authenticator } from "@aws-amplify/ui-react";
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>CI/CD TEST update build in gitignore</p>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => {
+        console.log("signOut, user", signOut, user);
+        return (
+          <main>
+            <h1>Hello {user.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        );
+      }}
+    </Authenticator>
   );
 }
 
